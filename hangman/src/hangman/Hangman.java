9b9c0576e8bee1a8ae017973c09d3070138f9af9
Iyad Shaheen <ivs1990@yahoo.com>
2018-11-08 01:41:28 -0800

    made the panel bigger

--- hangman/src/hangman/Hangman.java
+++ hangman/src/hangman/Hangman.java
@@ -15,7 +15,7 @@ public class Hangman extends Application {
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("Hangman.fxml"));
 		loader.setController(new GameController(game));
 		Parent root = loader.load();
-		Scene scene = new Scene(root, 500, 600);
+		Scene scene = new Scene(root, 530, 600);
 		scene.getStylesheets().add(getClass().getResource("Hangman.css").toExternalForm());
 		primaryStage.setScene(scene);
 		primaryStage.show();
